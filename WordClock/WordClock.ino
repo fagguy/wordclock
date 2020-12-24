9dba721e8d500ae7d03be23b594b7f29d3299044
isaiahl <lim.isaiah@gmail.com>
2020-12-24 14:59:18 +0800

    Add basic NTP functionality for testing

--- WordClock/WordClock.ino
+++ WordClock/WordClock.ino
@@ -1,11 +1,35 @@
 #include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#include <NTPClient.h>
+
+// WiFi credentials
+const char *ssid      = "<SSID>";
+const char *password  = "<PASSWORD>";
+
+// NTP parameters
+const long utcOffsetInSeconds       = 8 * 60 * 60; // Singapore UTC +8 offset
+const unsigned long updateInterval  = 60 * 60 * 1000; // 1 hr
+
+// Create NTPClient instance
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "sg.pool.ntp.org", utcOffsetInSeconds, updateInterval);
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
 
+  while ( WiFi.status() != WL_CONNECTED ) {
+    delay ( 500 );
+    Serial.print ( "." );
+  }
+  Serial.println ( "Connected" );
+
+  timeClient.begin();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  timeClient.update();
+  timeClient.getFormattedTime();
 }
